c3b9f323b29e9376b0fa6b5b26df7334a2a707a5
andela-ooshodi <olufunmilade.oshodi@andela.com>
2016-05-18 23:35:51 +0100

    [Chore] Initial setup

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
+var Home = React.createClass({
+    render: function() {
+        return <h1>Hello World</h1>
+    }
+});
+
+module.exports = Home;
